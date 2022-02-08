392c5a5f4c4d3c33b25c4ca2814096cfdaa6335c
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2022-02-08 20:51:58 +0800

    output is in pdf

--- PDF_resume.py
+++ PDF_resume.py
@@ -3,4 +3,8 @@ import json
 
 data_json= open ('resume.json')
 resume_raw=json.loads(data_json.read())
-print(resume_raw)
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size=16)
+pdf.cell(txt=resume_raw["BASIC INFORMATION"]["NAME"], ln=1)
+pdf.output("test.pdf")
