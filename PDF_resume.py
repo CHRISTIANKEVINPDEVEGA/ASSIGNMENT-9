79e863076a734eef8b57f6f9f0f92d40be67b890
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2022-02-11 23:32:36 +0800

    finished

--- PDF_resume.py
+++ PDF_resume.py
@@ -1,5 +1,6 @@
 from email.mime import image
 from multiprocessing import managers
+from re import T
 from tkinter import N
 from fpdf import FPDF
 import json
@@ -16,11 +17,10 @@ ADDRESS=resume_raw["CONTACT"]["ADDRESS"]
 EMAIL=resume_raw["CONTACT"]["EMAIL"]
 PHONE_NUM=resume_raw["CONTACT"]["PHONE NUMBER"]
 FB=resume_raw["CONTACT"]["FACEBOOK"]
-
 PRIM_EDUC=resume_raw["EDUC BACKGROUND"]["PRIMARY EDUCATION"]
 SEC_EDUC=resume_raw["EDUC BACKGROUND"]["SECONDARY EDUCATION"]
 TER_EDUC=resume_raw["EDUC BACKGROUND"]["TERTIARY EDUCATION"]
-AWARDS_ACCOMP=1
+AWARDS_ACCOMP=resume_raw["EDUC BACKGROUND"]["AWARDS AND ACCOMPLISHMENTS"]
 PERSONAL_SKILLS=resume_raw["SKILLS AND GOALS"]["PERSONAL SKILLS"]
 GOALS=resume_raw["SKILLS AND GOALS"]["GOALS"]
 JOB1=resume_raw["CAREER BACKGROUND"]["JOB1"]
@@ -29,78 +29,68 @@ JOB2=resume_raw["CAREER BACKGROUND"]["JOB2"]
 
 
 pdf = FPDF('P','mm','A4')
-pdf.set_top_margin(20)
-pdf.set_left_margin(19)
+pdf.set_top_margin(25)
+pdf.set_left_margin(15)
 pdf.set_right_margin(15)
 pdf.add_page()
 
-
-pdf.image("bg.png",0 ,0,216,297)
+#For the background,the picture and txt color
+pdf.image("bg.png",0 ,0,210,297)
+pdf.set_fill_color(73,117,156)
+pdf.set_text_color(1,1,1)
 pdf.set_font('Times','B', size=15)
-pdf.set_text_color(255,25,60)
-pdf.set_fill_color(223,220,213)
-pdf.cell(90, 187,txt= "Image", ln=0,border=1,align="B",fill=1)
+pdf.cell(90, 112,ln=0,border=0,fill=1)
+pdf.image("boi.jpg",20 ,40,80,80)
 
-pdf.image("boi.jpg",19 ,20,80,80)
-pdf.set_font('Times','B', size=15)
-pdf.set_text_color(220,50,50)
 
+#BASIC INFORMATION
+pdf.cell(90, 6,txt= "BASIC INFORMATION", ln=2,border="B",align="L",fill=0)
+pdf.set_font('arial','B', size=8)
+pdf.cell(90, 6,txt="Name: " + NAME, ln=2,border=0,align="L",fill=0)
+pdf.cell(90, 6,txt="Age: " + AGE, ln=2,border=0,align="L",fill=0)
+pdf.cell(90, 6,txt="Birthday: " + BIRTHDAY, ln=2,border=0,align="L",fill=0)
+pdf.multi_cell(90, 6,txt="Personality:\n" + PERSONALITY, ln=2,border=0,align="L",fill=0)
+
+#EDUCATION BACKGROUND
 pdf.set_font('Times','B', size=15)
-pdf.set_text_color(220,50,50)
-pdf.cell(90, 5,txt= "BASIC INFORMATION", ln=2,border=1,align="L",fill=0)
-pdf.set_text_color(100,50,50)
+pdf.cell(90,6,txt= "EDUCATION BACKGROUND", ln=2,border="B",align="L",fill=0)
+pdf.set_font('arial','B', size=8)
+pdf.cell(90,6,txt= "Primary Education", ln=2,border="B",align="L",fill=0)
+pdf.multi_cell(90,6,txt=PRIM_EDUC, ln=2,border=0,align="L",fill=0)
+pdf.cell(90,6,txt= "Secondary Education", ln=2,border="B",align="L",fill=0)
+pdf.multi_cell(90,6,txt=SEC_EDUC, ln=2,border=0,align="L",fill=0)
+pdf.cell(90,6,txt= "Tertiary Education", ln=2,border="B",align="L",fill=0)
+pdf.multi_cell(90,6,txt= TER_EDUC, ln=1,border=0,align="L",fill=0)
+
+#Awards and Accomplishments
+pdf.cell(180,6,txt= "Awards and Accomplishments", ln=2,border="B",align="L",fill=0)
+pdf.multi_cell(180,6,txt= AWARDS_ACCOMP, ln=1,border=0,align="L",fill=0)
+
+#Skills and Goals
+pdf.set_font('Times','B', size=15)
+pdf.cell(180,6,txt= "Skills and Goals", ln=2,border="B",align="L",fill=0)
+pdf.set_font('arial','B', size=8)
+pdf.cell(180,5,txt= "Skills", ln=2,border="B",align="L",fill=0)
+pdf.multi_cell(180, 6,txt= PERSONAL_SKILLS ,ln=2,border=0,align="L",fill=0)
+pdf.cell(180,6,txt= "Goals", ln=2,border="B",align="L",fill=0)
+pdf.multi_cell(180,6,txt= GOALS,ln=1,border=0,align="L",fill=0)
+
+#Career Background
+pdf.set_font('Times','B', size=15)
+pdf.cell(180, 6,txt= "Career Background", ln=1,border=0,align="L",fill=0)
+pdf.set_font('arial','B', size=8)
+pdf.multi_cell(180, 6,txt=JOB1,ln=1,border="T",align="L",fill=0)
+pdf.multi_cell(180, 6,txt=JOB2,ln=1,border="T",align="L",fill=0)
+
+#CONTACT
+pdf.set_font('Times','B', size=15)
+pdf.cell(180, 6,txt= "CONTACT", ln=1,border="B",align="L",fill=0)
 pdf.set_font('arial','', size=8)
-pdf.cell(90, 7,txt="Name: " + NAME, ln=2,border=1,align="L",fill=0)
-pdf.cell(90, 7,txt="Age: " + AGE, ln=2,border=1,align="L",fill=0)
-pdf.cell(90, 7,txt="Birthday: " + BIRTHDAY, ln=2,border=1,align="L",fill=0)
-pdf.multi_cell(90, 7,txt="Personality:\n" + PERSONALITY, ln=2,border=1,align="L",fill=0)
-
-
-pdf.set_font('Times','B', size=15)
-pdf.set_text_color(220,50,50)
-pdf.cell(90,7,txt= "EDUCATION BACKGROUND", ln=2,border=1,align="L",fill=0)
-pdf.set_text_color(100,50,50)
-pdf.set_font('arial','', size=8)
-pdf.multi_cell(90,7,txt= "Primary Education:\n" + PRIM_EDUC, ln=2,border=1,align="L",fill=0)
-pdf.multi_cell(90,7,txt= "Secondary Education:\n"+ SEC_EDUC, ln=2,border=1,align="L",fill=0)
-
-
-pdf.multi_cell(90,7,txt= "Tertiary Education\n" + TER_EDUC, ln=2,border=1,align="L",fill=0)
+pdf.cell(180,5,txt="Address: " + ADDRESS, ln=1,border=0,align="L",fill=0)
+pdf.cell(180,6,txt="Email: " + EMAIL, ln=1,border=0,align="L",fill=0)
+pdf.cell(180,5,txt="Phone number: " + PHONE_NUM, ln=1,border=0,align="L",fill=0)
+pdf.cell(180,6,txt="Facebook: " + FB, ln=0,border=0,align="L",fill=0)
 
 
 
-
-pdf.set_font('Times','B', size=15)
-pdf.set_text_color(220,50,50)
-pdf.cell(90,7,txt= "Skills and Goals", ln=2,border=1,align="L",fill=0)
-pdf.set_text_color(100,50,50)
-pdf.set_font('arial','', size=8)
-pdf.cell(90,7,txt= "Skills", ln=2,border=1,align="L",fill=0)
-pdf.multi_cell(90, 7,txt=resume_raw["SKILLS AND GOALS"]["PERSONAL SKILLS"],ln=2,border=1,align="L",fill=0)
-pdf.cell(90,7,txt= "Goals", ln=2,border=1,align="L",fill=0)
-pdf.multi_cell(90,7,txt=resume_raw["SKILLS AND GOALS"]["GOALS"],ln=1,border=1,align="L",fill=0)
-
-pdf.set_font('Times','B', size=15)
-pdf.set_text_color(220,50,50)
-pdf.cell(180, 7,txt= "Career Background", ln=1,border=1,align="L",fill=0)
-pdf.set_text_color(100,50,50)
-pdf.set_font('arial','', size=8)
-pdf.multi_cell(180, 7,txt=JOB1,ln=1,border=1,align="L",fill=0)
-pdf.multi_cell(180, 7,txt=JOB2,ln=1,border=1,align="L",fill=0)
-
-
-
-pdf.set_font('Times','B', size=15)
-pdf.set_text_color(220,50,50)
-pdf.cell(180, 7,txt= "CONTACT", ln=1,border=1,align="L",fill=0)
-pdf.set_text_color(100,50,50)
-pdf.set_font('arial','', size=8)
-pdf.cell(180,7,txt="Address: " + ADDRESS, ln=1,border=1,align="L",fill=0)
-pdf.cell(180,7,txt="Email: " + EMAIL, ln=1,border=1,align="L",fill=0)
-pdf.cell(180,7,txt="Phone number: " + PHONE_NUM, ln=1,border=1,align="L",fill=0)
-pdf.cell(180,7,txt="Facebook: " + FB, ln=0,border=1,align="L",fill=0)
-
-
-
-FB
 pdf.output("test2.pdf")
